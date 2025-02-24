bb6a53d97701a0a722846994f23b1c87e24ba5b6
luizlopesbr <86674476+luizlopesbr@users.noreply.github.com>
2025-02-24 12:40:22 -0300

    Update userWorkSpace.js
    Exclusão de comentário desnecessário.

--- pages/js/userWorkSpace.js
+++ pages/js/userWorkSpace.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const userName = localStorage.getItem("username"); // 🔹 Pega o nome do usuário
     if (userName) {
-        //document.querySelector(".imgPerfil p").textContent = userName;
         document.querySelector(".userName").textContent = `${userName}, Bem vindo!`;
     }
 });
